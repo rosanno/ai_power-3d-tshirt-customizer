e6a4c83afb58c40f67d9ba2dce3d3c7e022a71c5
rosanno <rosannomacabuhay@gmail.com>
2023-06-20 20:53:16 +0800

    updated

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import dalleRoutes from "./routes/dalle.routes.js";
 dotenv.config();
  
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
