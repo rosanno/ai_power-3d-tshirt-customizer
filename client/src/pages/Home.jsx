82f81899fe45dd352d364765dbb9153a01be15f7
rosanno <rosannomacabuhay@gmail.com>
2023-06-20 21:01:30 +0800

    Updated bg

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,11 +18,7 @@ const Home = () => {
       {snap.intro && (
         <motion.div className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
-            <img
-              src="./threejs.png"
-              alt="logo"
-              className="w-8 h-8 object-contain"
-            />
+            <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain" />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
@@ -31,21 +27,17 @@ const Home = () => {
                 Let's <br className="x:block hidden" /> DO IT.
               </h1>
             </motion.div>
-            <motion.div
-              {...headContentAnimation}
-              className="flex flex-col gap-4"
-            >
-              <p className="max-w-md font-normal text-gray-600 text-base">
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool. <strong>Unleash your imagiation</strong> and
-                define your own style.
+            <motion.div {...headContentAnimation} className="flex flex-col gap-4">
+              <p className="max-w-md font-normal text-gray-300 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D customization tool.{" "}
+                <strong>Unleash your imagiation</strong> and define your own style.
               </p>
 
-              <CustomButton 
-                 type="filled"
-                 title="Customize It"
-                 handleClick={() => state.intro = false}
-                 customStyle="w-fit px-4 py-2.5 font-bold text-sm"
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyle="w-fit px-4 py-2.5 font-bold text-sm"
               />
             </motion.div>
           </motion.div>
